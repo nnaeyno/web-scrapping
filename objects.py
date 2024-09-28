4515d3d3e5b23d47c97360100228c4b3da57f229
nnaeyno <ngogo20@freeuni.edu.ge>
2024-09-28 16:36:27 +0400

    recipe obj

--- objects.py
+++ objects.py
@@ -9,6 +9,7 @@ class Ingredient:
             "quantity": self.quantity
         }
 
+
 class Step:
     def __init__(self, step_number: int, description: str):
         self.step_number = step_number
@@ -20,11 +21,31 @@ class Step:
             "description": self.description
         }
 
+
 class Recipe:
-    def __init__(self, name: str, ingredients: list[Ingredient], steps: list[Step]):
+    """         რეცეპტის დასახელება
+                რეცეპტის მისამართი (URL თვითონ რეცეპტის)
+                რეცეპტის მთავარი კატეგორიის დასახელება და მისამართი(URL)
+                რეცეპტის ქვეკატეგორიiს დასახელება და მისამართი(URL)
+                მთავარი სურათის მისამართი
+                მოკლე აღწერა( გარედან რეცეპტს რაც აქვს - რეცეპტის დეტალურიდანაც შეიძლება მაგ ინფოს აღება)
+                ავტორი სახელი
+                ულუფების რაოდენობა
+                რეცეპტის ინგრედიენტები
+                რეცეპტის მომზადების ეტაპები"""
+    def __init__(self, name: str, ingredients: list[Ingredient], steps: list[Step], category: str, subcategory: str,
+                 image_url: str, description: str, author_name: str, portions: int):
         self.name = name
         self.ingredients = ingredients
         self.steps = steps
+        self.author_name = author_name
+        self.subcategory = subcategory
+        self.description = description
+        self.image_url = image_url
+        self.category = category
+        self.subcategory = subcategory
+        self.portions = portions
+
 
     def to_dict(self):
         return {
